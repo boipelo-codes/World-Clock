98905b991fcce2ee4208b0d11b39082558586199
Boipelo Ngakane <boipelongakane16@gmail.com>
2025-03-24 01:25:52 +0200

    updated js

--- javascript/index.js
+++ javascript/index.js
@@ -4,5 +4,10 @@ function updateTime() {
         let johannesburgDateElement = johannesburgElement.querySelector(".date");
         let johannesburgTimeElement = johannesburgElement.querySelector(".time");
         let johannesburgTime = moment().tz ("Africa/Johannesburg");
+
+        johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM	Do YYYY");
+        johannesburgTimeElement.innerHTML = johannesburgTime.format(
+          "h:mm:ss [<small>]A[</small>]"
+        );
+      }
     }
-}
