facf659e1c5423c4902c4d7a9fb7d2d06d8db97d
Boipelo Ngakane <boipelongakane16@gmail.com>
2025-03-24 02:09:59 +0200

    updated js

--- javascript/index.js
+++ javascript/index.js
@@ -12,6 +12,53 @@ function updateTime() {
     }
 }
 
+    let tokyoElement = document.querySelector("#tokyo");
+    if (tokyoElement) {
+        let tokyoDateElement = tokyoElement.querySelector(".date");
+        let tokyoTimeElement = tokyoElement.querySelector(".time");
+        let tokyoTime = moment().tz("");
+
+        tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+        tokyoTimeElement.innerHTML = tokyoTime.format(
+            "h:mm:ss [<small>]A[</small>]"
+        );
+    }
+
+    let astanaElement = document.querySelector("#astana");
+    if (astanaElement) {
+        let astanaDateElement = astanaElement.querySelector(".date");
+        let astanaTimeElement = astanaElement.querySelector(".time");
+        let astanaTime = moment().tz("");
+
+        astanaDateElement.innerHTML = astanaTime.format("MMMM Do YYYY");
+        astanaTimeElement.innerHTML = astanaTime.format(
+            "h:mm:ss [<small>]A[</small>]"
+        );
+    }
+
+    function updateCityTime(cityTimeZone) {
+        let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+        let cityTime = moment().tz(cityTimeZone);
+        let citiesElement = document.querySelector("#cities");
+      
+        if (citiesElement) {
+          citiesElement.innerHTML = `
+            <div class="city">
+              <div>
+                <h2>${cityName}</h2>
+                <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+              </div>
+              <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+            </div>
+          `;
+        }
+      }
+      
+
+
 updateTime();
 
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
